cbc09c2868292ca4b54bbbc9d091728991d18611
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2021-09-04 13:04:08 +0600

    Update

--- pages/views.py
+++ pages/views.py
@@ -4,29 +4,29 @@ from django.shortcuts import render
 
 class HomepageView(View):
     def get(self, request):
-        return render(request, 'homepage/index.html')
+        return render(request, 'pages/index.html')
 
 
 class GuideView(View):
     def get(self, request):
-        return render(request, 'guide/guide.html')
+        return render(request, 'pages/guide.html')
 
 
 class FAQView(View):
     def get(self, request):
-        return render(request, 'faq/FAQ.html')
+        return render(request, 'pages/FAQ.html')
 
 
 class GetBoozeView(View):
     def get(self, request):
-        return render(request, 'get_booze/get_booze.html')
+        return render(request, 'pages/get_booze.html')
 
 
 class BoozItUpView(View):
     def get(self, request):
-        return render(request, 'booz-it-up/booz-it-up.html')
+        return render(request, 'pages/booz-it-up.html')
 
 
 class WhatIsBoozecoinView(View):
     def get(self, request):
-        return render(request, 'what-is-boozecoin/what-is-Boozecoin.html')
+        return render(request, 'pages/what-is-Boozecoin.html')
